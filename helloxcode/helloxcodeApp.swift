a6a36a1c75670712e630787dad507ede4dd3b25e
Kohn Liang <jiankun.liang@accenture.com>
2024-09-02 18:33:34 +0800

    Initial Commit

---
+++ helloxcode/helloxcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloxcodeApp.swift
+//  helloxcode
+//
+//  Created by jiankun.liang on 2024/9/2.
+//
+
+import SwiftUI
+
+@main
+struct helloxcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
